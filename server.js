9cb0d424ec97da3a3ac92d2ad3dff412d4325417
unknown <liuy31@mysoft.com.cn>
2018-03-31 16:20:11 +0800

    'init'

---
+++ server.js
@@ -0,0 +1,10 @@
+const egg = require('egg');
+let port  = 80;
+let processNum = 1;
+const workers = Number(processNum || process.argv[2] || require('os').cpus().length);
+console.log(workers)
+egg.startCluster({
+  port,
+  workers,
+  baseDir: __dirname,
+});
